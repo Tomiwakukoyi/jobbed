ef47ca9d41406a1e2b12cf1e9f302e1d2d1e0bbb
Tomiwa Kukoyi <tomiwakukoyiweb@gmail.com>
2023-11-14 12:38:34 +0100

    First commit for jobbed

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import styles from "./welcome.style";
+import { useRouter } from "expo-router";
+import { icons, SIZES } from "../../../constants";
+
+const Welcome = () => {
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  );
+};
+
+export default Welcome;
